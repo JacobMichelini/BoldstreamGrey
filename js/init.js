c4f5d3e408b2f4c53c0f4e22bab3eaf2f82e602f
JacobMichelini <devoq81@gmail.com>
2015-07-07 13:56:33 -0700

    misc+added live page

--- js/init.js
+++ js/init.js
@@ -6,7 +6,8 @@
     $('.scrollspy').scrollSpy();
 
     var options = [
-        {selector: '#bartrigger', offset: 250, callback: '$(".seethru").velocity({ backgroundColor: "#000", backgroundColorAlpha: 0.85}, {duration: 400}); ' }
+        {selector: '#bartrigger', offset: 250, callback: '$(".seethru").velocity({ backgroundColor: "#000", backgroundColorAlpha: 0.85}, {duration: 400}); ' },
+        {selector: '#live-list', offset: 100, callback: 'Materialize.showStaggeredList("#live-list")' }
       ];
       Materialize.scrollFire(options);
 
